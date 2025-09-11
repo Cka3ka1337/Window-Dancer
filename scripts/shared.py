5bc8a17c943b5c082736f76a32dca3b33814bbe5
Cka3ka1337 <den.steam20074@mail.ru>
2025-09-11 18:48:29 +0300

    Added shared temp class

---
+++ scripts/shared.py
@@ -0,0 +1,48 @@
+from PySide6.QtCore import Signal, QObject
+
+
+class SharedData(QObject):
+    __instance = None
+    change_event = Signal(tuple)
+    _shared_data = {}
+    
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(SharedData, cls).__new__(cls)
+            cls.__instance._shared_data = {}
+            
+        return cls.__instance
+    
+    
+    def __init__(self):
+        if not hasattr(self, '_initialized'):
+            super().__init__()
+            self._initialized = True
+            self._shared_data = {}
+    
+    
+    def get(self, path: str):
+        path = path.split('.')
+        result = self._shared_data
+        
+        for p in path:
+            result = result.get(p)
+            
+            if result is None:
+                return None
+        
+        return result
+    
+
+    def set(self, path: str, value):
+        _path = path.split('.')
+        result = self._shared_data
+        
+        for p in _path[:-1]:
+            if p not in result:
+                result[p] = {}
+            
+            result = result[p]
+        
+        result[_path[-1]] = value
+        self.change_event.emit((path, value))
